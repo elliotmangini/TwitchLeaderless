143c0eeda5180e7b0a5a1d264a63fc9e8e1ea368
Elliot Mangini <elliot.mangini@gmail.com>
2023-04-06 21:14:36 -0600

    connect script to GUI

--- popup.js
+++ popup.js
@@ -1,2 +1,12 @@
-const panel = document.querySelector("#root > div > div.Layout-sc-1xcs6mc-0.htmBdw > div > div > section > div > div:nth-child(1)");
-panel.style.display = "none";
+document.addEventListener('DOMContentLoaded', function() {
+    var checkPageButton = document.getElementById('checkPage');
+    checkPageButton.addEventListener('click', function() {
+  
+      chrome.tabs.getSelected(null, function(tab) {
+
+        const panel = document.querySelector("#root > div > div.Layout-sc-1xcs6mc-0.htmBdw > div > div > section > div > div:nth-child(1)");
+        panel.style.display = "none";
+
+      });
+    }, false);
+  }, false);
